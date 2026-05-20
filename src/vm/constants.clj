3d114e3d3c0d36d7ec156c410cd44d2d69dca045
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-05-21 00:14:39 +0300

    feat(core): updated instructions list

--- src/vm/constants.clj
+++ src/vm/constants.clj
@@ -4,15 +4,25 @@
 ;; Architecture constants
 ;; ==============================
 
-(def MEMORY-SIZE 65536)                                     ; memory size - 64k words
-(def REG-COUNT 16)                                          ; registers count - 16
-(def INSTR-SIZE 4)                                          ; word size - 4
-
-; Special addresses for memory-mapped I/O
-(def IO-READ-ADDR 0xFFFFFFF0)                               ; reading a symbol
-(def IO-WRITE-ADDR 0xFFFFFFF4)                              ; writing a symbol
-(def IO-STATUS-ADDR 0xFFFFFFF8)                             ; device status
+;; Memory
+(def MEMORY-SIZE 65536)                                     ; 64K words
+(def WORD-SIZE 32)                                          ; 32 bits per word
 
 
-; Address of the interrupt vector table
-(def TRAP-VECTOR-TABLE 0x00000100)
+;; Registers
+(def REG-COUNT 16)                                          ; r0-r15, r0 is always zero
+(def LINK-REGISTER 15)                                      ; r15 is used as link register for jal
+
+
+;; Instruction encoding
+(def OPCODE-SIZE 5)                                         ; 5 bits for opcode (32 possible instructions)
+(def OPCODE-SHIFT 27)                                       ; 32 - 5 = 27 (shift amount for encoding)
+
+
+;; Special addresses for memory-mapped I/O
+(def IO-READ-ADDR 0xFFFFFFF0)                               ; reading a character
+(def IO-WRITE-ADDR 0xFFFFFFF4)                              ; writing a character
+(def IO-STATUS-ADDR 0xFFFFFFF8)                             ; device status (0 = busy, 1 = ready)
+
+;; Interrupts
+(def TRAP-VECTOR-TABLE 0x00000100)                          ; where trap handlers start
--- src/vm/opcodes.clj
+++ src/vm/opcodes.clj
@@ -5,22 +5,28 @@
 ;; ==============================
 
 (def opcodes
-  {:nop  2r0000                                             ; no operation
-   :add  2r0001                                             ; add  rd, rs, rt   => rd = rs + rt
-   :sub  2r0010                                             ; sub  rd, rs, rt   => rd = rs - rt
-   :mul  2r0011                                             ; mul  rd, rs, rt   => rd = rs * rt
-   :div  2r0100                                             ; div  rd, rs, rt   => rd = rs / rt
-   :and  2r0101                                             ; and  rd, rs, rt   => rd = rs & rt
-   :or   2r0110                                             ; or   rd, rs, rt   => rd = rs | rt
-   :xor  2r0111                                             ; xor  rd, rs, rt   => rd = rs ^ rt
-   :lw   2r1000                                             ; lw   rd, addr     => rd = mem[addr]
-   :sw   2r1001                                             ; sw   rs, addr     => mem[addr] = rs
-   :beq  2r1010                                             ; beq  rs, rt, off  => if rs == rt: pc += off
-   :bne  2r1011                                             ; bne  rs, rt, off  => if rs != rt: pc += off
-   :j    2r1100                                             ; j    addr         => pc = addr
-   :jal  2r1101                                             ; jal  addr         => r15 = pc, pc = addr (link reg)
-   :trap 2r1110                                             ; trap code         => system call
-   :hlt  2r1111                                             ; halt              => stopping the processor
+  {:nop  2r00000                                            ; no operation
+   :add  2r00001                                            ; add  rd, rs, rt   => rd = rs + rt
+   :sub  2r00010                                            ; sub  rd, rs, rt   => rd = rs - rt
+   :mul  2r00011                                            ; mul  rd, rs, rt   => rd = rs * rt
+   :div  2r00100                                            ; div  rd, rs, rt   => rd = rs / rt
+   :and  2r00101                                            ; and  rd, rs, rt   => rd = rs & rt
+   :or   2r00110                                            ; or   rd, rs, rt   => rd = rs | rt
+   :xor  2r00111                                            ; xor  rd, rs, rt   => rd = rs ^ rt
+   :lw   2r01000                                            ; lw   rd, addr     => rd = mem[addr]
+   :sw   2r01001                                            ; sw   rs, addr     => mem[addr] = rs
+   :beq  2r01010                                            ; beq  rs, rt, off  => if rs == rt: pc += off
+   :bne  2r01011                                            ; bne  rs, rt, off  => if rs != rt: pc += off
+   :blt  2r01100                                            ; blt  rs, rt, off  => if rs <  rt: pc += off
+   :bgt  2r01101                                            ; bgt  rs, rt, off  => if rs >  rt: pc += off
+   :ble  2r01110                                            ; ble  rs, rt, off  => if rs <= rt: pc += off
+   :bge  2r01111                                            ; bge  rs, rt, off  => if rs >= rt: pc += off
+   :bltz 2r10000                                            ; bltz rs, off      => if rs <  0:  pc += off
+   :bgtz 2r10001                                            ; bgtz rs, off      => if rs >  0:  pc += off
+   :j    2r10010                                            ; j    addr         => pc = addr
+   :jal  2r10011                                            ; jal  addr         => r15 = pc, pc = addr (link reg)
+   :trap 2r10100                                            ; trap code         => system call
+   :hlt  2r10101                                            ; halt              => stopping the processor
    })
 
 (def opcode->mnemonic (zipmap (vals opcodes) (keys opcodes)))
