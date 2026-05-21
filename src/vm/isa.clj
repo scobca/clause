cc494992e0a15936ba4951b11c7a555cc7ad7c9c
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-05-22 01:17:38 +0300

    feat(isa): updated encoders comments

--- src/vm/isa.clj
+++ src/vm/isa.clj
@@ -1,6 +1,6 @@
 (ns vm.isa
-  [:require [vm.opcodes :refer [opcodes]]]
-  [:require [vm.constants :refer [OPCODE-SHIFT]]])
+  [:require [vm.opcodes :refer [opcodes opcode->mnemonic]]]
+  [:require [vm.constants :refer [MEMORY-SIZE REG-COUNT OPCODE-SHIFT]]])
 
 
 ;; R-type: [op:5][rd:5][rs:5][rt:5][unused:12]
@@ -13,16 +13,16 @@
 
 
 ;; I-type: [op:5][rd/rs:5][rt:5][imm:17]
-(defn encode-i-type
-  [op a b imm]
+(defn encode-i-type [op a b imm]
   (let [opcode (get opcodes op)]                            ; get current opcode number
     (bit-or (bit-shift-left opcode OPCODE-SHIFT)            ; set opcode num to bits 31-27
             (bit-shift-left a 22)                           ; set A to bits 26-22
             (bit-shift-left b 17)                           ; set B to bits 21-17
             (bit-and imm 0x1FFFF))))                        ; bits 16-0: immediate (17 bits)
 
+
 ;; J-type: [op:5][addr:27]
 (defn encode-j-type [op addr]
   (let [opcode (get opcodes op)]                            ; get current opcode number
     (bit-or (bit-shift-left opcode OPCODE-SHIFT)            ; set opcode num to bits 31-27
-            (bit-and addr 0x7FFFFFF))))
+            (bit-and addr 0x7FFFFFF))))                     ; bits 26-0: address (27 bits)
